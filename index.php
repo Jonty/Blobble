2792b077a533ca517bbc4d60232f63a4a7c379a6
Jonty <jonty@jonty.co.uk>
2010-01-30 23:37:28 +0000

    Prettify the interface a bit

--- index.php
+++ index.php
@@ -16,14 +16,72 @@
         exit;
     }
 
-    if (isset($_GET['mac'])) {
-        if (preg_match('/([\w\d]+:?)+/', trim($_GET['mac']))) {
-            $oSession->setUserMac($user, trim($_GET['mac']));
-            print "<h4>MAC set</h4><hr>";
-        } else {
-            print "<h4>Invalid MAC!</h4><hr>";
-        }
-    }
+?>
+<html>
+    <head>
+        <title>Blobble</title>
+        <style type="text/css">
+            body {
+                font-family: Helvetica, Bitstream Vera Sans, sans-serif;
+                color: #000000;
+            }
+            .content {
+                text-align: left;
+                margin-left: 20%;
+                margin-top: 8%;
+            }
+            .heading {
+                font-size: 3em;
+                font-weight: bold;
+            }
+            A:link {
+                text-decoration: none;
+                color: #008000;
+            }
+            A:visited {
+                text-decoration: none;
+                color: #008000;
+            }
+            A:active {
+                text-decoration: none;
+                color: #008000;
+            }
+            A:hover {
+                text-decoration: underline;
+                color: #008000;
+            }
+        </style>
+    </head>
+    <body>
+    <body>
+        <div><a href="?logout=1">Not <?=htmlentities($aUser['name'])?>?</a></div>
+        <div class="content">
+<?
+            if (isset($_GET['mac'])) {
+                if (preg_match('/([a-fA-F0-9]+:?)+/', trim($_GET['mac']))) {
+                    $oSession->setUserMac($user, trim($_GET['mac']));
+                    $aUser = $oSession->getUserByName($user);
+                } else {
+                    print "Invalid MAC address!";
+                }
+            }
+?>
+            <div class="heading">Blobble</div>
+            <div class="item">
+                <form>
+                    Your bluetooth Mac address:<br>
+                    <input name="mac" type="text" value="<?=htmlentities($aUser['mac'])?>">
+                    <input type="submit" value="Save MAC">
+                </form>
+            </div>
+            <div class="item" style="width:40%; margin-top: 1%">
+                <small>
+                Once you've entered your bluetooth MAC address, songs you hear playing on Blobble enabled computers will automatically be scrobbled to your Last.fm profile.
+                </small>
+            </div>
+        </div>
+    </body>
+</html>
+<?
 
-    print "Blobble! Logged in as '$user' (<a href='?logout=1'>logout</a>)<br><br>";
-    print "Your phone bluetooth MAC address: <form><input name='mac' type='text' value='".htmlentities($aUser['mac'])."'><input type='submit' value='Save MAC'></form>";
+
